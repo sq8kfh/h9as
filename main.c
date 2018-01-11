c079c67882a604bfd6ca7457ad9396c86bcc8963
SQ8KFH <sq8kfh@gmail.com>
2018-01-11 20:32:05 +0100

    ...

--- main.c
+++ main.c
@@ -11,34 +11,16 @@
 
 #include "can/can.h"
 
-volatile uint8_t output1 = 0;
-volatile uint8_t output2 = 0;
+volatile uint8_t output = 0;
 	
 #define LED_DDR DDRB
 #define LED_PORT PORTB
 #define LED_PIN PB1
 
 void
-set_output(uint8_t o1, uint8_t o2)
+set_output(uint8_t o)
 {
-	if (o1) {
-		PORTB |= _BV(PB5);
-		output1 = 1;
-	}
-	else {
-		PORTB &= ~_BV(PB5);
-		output1 = 0;
-	}
-	if (o2) {
-		PORTB |= _BV(PB6);
-		output2 = 1;
-	}
-	else {
-		PORTB &= ~_BV(PB6);
-		output2 = 0;
-	}
-		
-	/*switch (o) {
+	switch (o) {
 		case 1:
 			PORTC |= _BV(PC7);
 			PORTB &= ~_BV(PB5);
@@ -128,9 +110,10 @@ set_output(uint8_t o1, uint8_t o2)
 			PORTC &= ~_BV(PC0);
 			PORTD &= ~_BV(PD1);
 			PORTC &= ~_BV(PC1);
-			return 0;
+            output = 0;
+			return;
 	}
-	return o;*/
+    output = o;
 }
 
 int main(void)
@@ -164,13 +147,13 @@ int main(void)
 		}
 		--led_counter;
 		
-		set_output(output1, output2);
+		set_output(output);
 		
 		h9msg_t cm;
 		if (CAN_get_msg(&cm)) {
 			LED_PORT |= (1<<LED_PIN);
 			led_counter = 0x10000;
-			if (cm.type == H9_TYPE_GET_REG) {
+			if (cm.type == H9MSG_TYPE_GET_REG) {
 				h9msg_t cm_res;
 				CAN_init_response_msg(&cm, &cm_res);
 				cm_res.dlc = 2;
@@ -180,80 +163,40 @@ int main(void)
                 uint16_t adc;
 				switch (cm.data[0]) {
 					case 10:
-						cm_res.data[1] = output1;
+						cm_res.data[1] = output;
 						CAN_put_msg(&cm_res);
 						break;
 					case 11:
-						cm_res.data[1] = output2;
-						CAN_put_msg(&cm_res);
-						break;
-					case 12:
                         ADMUX = (1 << MUX3) | (1 << MUX1);
 						ADCSRA |= (1<<ADSC); //ADSC: uruchomienie pojedynczej konwersji
 						cm_res.dlc = 3;
 						while(ADCSRA & (1<<ADSC)); //czeka na zako�czenie konwersji
                         tmp = ADCL;
                         adc = ADCH;
-						adc <<= 8;
+                        //cm_res.data[1] = adc;
+                        //cm_res.data[2] = tmp;
+                        adc <<= 8;
 						adc |= tmp;
-						tmp = (uint32_t)625*5*adc/256;
-						cm_res.data[1] = (uint8_t)((tmp >> 8) & 0xff);
+						tmp = (uint32_t)3125*5*adc/2304; //for Rl 18kOhm in 1/10 mA
+ 						cm_res.data[1] = (uint8_t)((tmp >> 8) & 0xff);
 						cm_res.data[2] = (uint8_t)(tmp & 0xff);
 						CAN_put_msg(&cm_res);
 						break;
-                    case 13:
-                        ADMUX = (1 << MUX3);
-                        ADCSRA |= (1<<ADSC); //ADSC: uruchomienie pojedynczej konwersji
-                        cm_res.dlc = 3;
-                        while(ADCSRA & (1<<ADSC)); //czeka na zako�czenie konwersji
-                        tmp = ADCL;
-                        adc = ADCH;
-                        adc <<= 8;
-                        adc |= tmp;
-                        tmp = 16*adc;
-                        cm_res.data[1] = (uint8_t)((tmp >> 8) & 0xff);
-                        cm_res.data[2] = (uint8_t)(tmp & 0xff);
-                        CAN_put_msg(&cm_res);
-                        break;
-                    case 14:
-                        ADMUX = (1 << MUX3) | (1 << MUX0);
-                        ADCSRA |= (1<<ADSC); //ADSC: uruchomienie pojedynczej konwersji
-                        cm_res.dlc = 3;
-                        while(ADCSRA & (1<<ADSC)); //czeka na zako�czenie konwersji
-                        tmp = ADCL;
-                        adc = ADCH;
-                        adc <<= 8;
-                        adc |= tmp;
-                        tmp = 16*adc;
-                        cm_res.data[1] = (uint8_t)((tmp >> 8) & 0xff);
-                        cm_res.data[2] = (uint8_t)(tmp & 0xff);
-                        CAN_put_msg(&cm_res);
-                        break;
 				}
 			}
-			else if (cm.type == H9_TYPE_SET_REG) {
+			else if (cm.type == H9MSG_TYPE_SET_REG) {
 				h9msg_t cm_res;
 				CAN_init_response_msg(&cm, &cm_res);
 				cm_res.dlc = 0;
 				cm_res.data[0] = cm.data[0];
 				switch (cm.data[0]) {
 					case 10:
-						set_output(cm.data[1], output2);
+						set_output(cm.data[1]);
 						_NOP();
 						_NOP();
 						_NOP();
 						_NOP();
-						cm_res.data[1] = output1;
-						cm_res.dlc = 2;
-						CAN_put_msg(&cm_res);
-						break;
-					case 11:
-						set_output(output1, cm.data[1]);
-						_NOP();
-						_NOP();
-						_NOP();
-						_NOP();
-						cm_res.data[1] = output2;
+						cm_res.data[1] = output;
 						cm_res.dlc = 2;
 						CAN_put_msg(&cm_res);
 						break;
@@ -261,6 +204,5 @@ int main(void)
 			}
 		}
 	}
-	return 0;
 }
 
